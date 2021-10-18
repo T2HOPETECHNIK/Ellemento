5cedb8f07a2df74a850155e9135f521a972841f4
jeff994 <hujianbo1981@gmail.com>
2021-10-18 17:45:48 +0800

    Added schedule for the bridge for each shelf

--- server_lib/ellemento/bridge/shelf_bridge.py
+++ server_lib/ellemento/bridge/shelf_bridge.py
@@ -86,5 +86,83 @@ class ShelfModelPlcBridge(ModelPlcBridge):
         register_address = self.modbus_io.config['address'][tag_name]
         res, error = self.modbus_io.write_system_coil(register_address, True)
         return res, error 
+    
+    def use_scheduler(self): 
+        tag_name = self.address['use_scheduler']['tag']
+        tag_position = self.address['use_scheduler']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_coil(register_address, tag_position, True)
+        return res, error
+
+    def set_pv_on_hh(self):
+        tag_name = self.address['pv_on_hh']['tag']
+        tag_position = self.address['pv_on_hh']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+    
+    def set_pv_on_mm(self): 
+        tag_name = self.address['pv_on_mm']['tag']
+        tag_position = self.address['pv_on_mm']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
+    def set_pv_off_hh(self): 
+        tag_name = self.address['pv_off_hh']['tag']
+        tag_position = self.address['pv_off_hh']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
+    def set_pv_off_mm(self): 
+        tag_name = self.address['pv_off_mm']['tag']
+        tag_position = self.address['pv_off_mm']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
+    def set_schedule_pv_value(self): 
+        tag_name = self.address['schedule_pv_value']['tag']
+        tag_position = self.address['schedule_pv_value']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
+    def set_light_on_hh(self): 
+        tag_name = self.address['light_on_hh']['tag']
+        tag_position = self.address['light_on_hh']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
+    def set_light_on_mm(self):
+        tag_name = self.address['light_on_mm']['tag']
+        tag_position = self.address['light_on_mm']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
+    def set_light_off_hh(self): 
+        tag_name = self.address['light_off_hh']['tag']
+        tag_position = self.address['light_off_hh']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
+    def set_light_off_mm(self): 
+        tag_name = self.address['light_off_mm']['tag']
+        tag_position = self.address['light_off_mm']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
+    def set_scheule_light_intensity(self):
+        tag_name = self.address['schedule_light_intensity']['tag']
+        tag_position = self.address['schedule_light_intensity']['position']
+        register_address = self.modbus_io.config['address'][tag_name]
+        res, error = self.modbus_io.write_register(register_address, tag_position, True)
+        return res, error
+
 
     
