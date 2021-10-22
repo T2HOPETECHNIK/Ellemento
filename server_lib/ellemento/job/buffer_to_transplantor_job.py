20f3901388380a5dae90329fee76a2063aa6214f
jeff994 <hujianbo1981@gmail.com>
2021-10-22 13:19:51 +0800

    Tested harvestor to buffer job

--- server_lib/ellemento/job/buffer_to_transplantor_job.py
+++ server_lib/ellemento/job/buffer_to_transplantor_job.py
@@ -9,7 +9,7 @@ import os
 from ellemento.model.tray import Tray
 from ellemento.model.tray_phase_1_3 import TrayPhase13
 from ellemento.model.tray_phase_4 import TrayPhase4
-from ellemento.model.transplantor import Transplantor
+from ellemento.model.transplantor import Transplantor, TransplantorType
 from ellemento.model.bufffer_factory import BufferFactory, BufferType
 from ellemento.model.transplantor_factory import TransplantorFactory
 from lib.logging.logger_initialiser import EllementoLogger
@@ -40,13 +40,85 @@ class BufferToTransplantorJob:
         except: 
             logger.info("Not able to create BufferToTransplantorJob")
 
+    @staticmethod 
+    def create_4_buffer_to_transplantor_job(): 
+        try: 
+            while not BufferToTransplantorJob.terminate_job: 
+                buffer_3_in = BufferFactory.get_buffer(BufferType.BUFFER_3_IN)
+                if not buffer_3_in.has_tray():
+                    logger.info("Buffer 3-in has not any trays")
+                    time.sleep(2)
+                    continue  
+
+                transplantor_3_4 = Transplantor(TransplantorFactory.get_transplator_3_4()) 
+                if transplantor_3_4.ready_to_move_in_src_tray(): 
+                    BufferToTransplantorJob.create_job(id = 1, src_buffer=buffer_3_in, dst_tranplantor=transplantor_3_4)
+                    logger.info("transplantor_3_4 is not ready to move in source tray")
+                time.sleep(2)
+                pass
+        except: 
+            logger.info("Not able to create BufferToTransplantorJob")
+        pass 
+
+    @staticmethod 
+    def create_3_buffer_to_transplantor_job():
+        try: 
+            while not BufferToTransplantorJob.terminate_job: 
+                buffer_3_in = BufferFactory.get_buffer(BufferType.BUFFER_3_IN)
+                if not buffer_3_in.has_tray():
+                    logger.info("Buffer 3-in has not any trays")
+                    time.sleep(2)
+                    continue  
+
+                transplantor_3_4 = Transplantor(TransplantorFactory.get_transplator_3_4()) 
+                if transplantor_3_4.ready_to_move_in_src_tray(): 
+                    BufferToTransplantorJob.create_job(id = 1, src_buffer=buffer_3_in, dst_tranplantor=transplantor_3_4)
+                    logger.info("transplantor_3_4 is not ready to move in source tray")
+                time.sleep(2)
+                pass
+        except: 
+            logger.info("Not able to create BufferToTransplantorJob")
+        pass 
+
     @staticmethod
     def create_5_buffer_to_transplantor_job():
+        try: 
+            while not BufferToTransplantorJob.terminate_job: 
+                buffer_3_in = BufferFactory.get_buffer(BufferType.BUFFER_3_IN)
+                if not buffer_3_in.has_tray():
+                    logger.info("Buffer 3-in has not any trays")
+                    time.sleep(2)
+                    continue  
+
+                transplantor_3_4 = Transplantor(TransplantorFactory.get_transplator_3_4()) 
+                if transplantor_3_4.ready_to_move_in_src_tray(): 
+                    BufferToTransplantorJob.create_job(id = 1, src_buffer=buffer_3_in, dst_tranplantor=transplantor_3_4)
+                    logger.info("transplantor_3_4 is not ready to move in source tray")
+                time.sleep(2)
+                pass
+        except: 
+            logger.info("Not able to create BufferToTransplantorJob")
         pass
 
     @staticmethod
     def create_4_in_buffer_to_transplantor_job():
-        pass 
+        try: 
+            while not BufferToTransplantorJob.terminate_job: 
+                buffer_3_in = BufferFactory.get_buffer(BufferType.BUFFER_3_IN)
+                if not buffer_3_in.has_tray():
+                    logger.info("Buffer 3-in has not any trays")
+                    time.sleep(2)
+                    continue  
+
+                transplantor_3_4 = Transplantor(TransplantorFactory.get_transplator_3_4()) 
+                if transplantor_3_4.ready_to_move_in_src_tray(): 
+                    BufferToTransplantorJob.create_job(id = 1, src_buffer=buffer_3_in, dst_tranplantor=transplantor_3_4)
+                    logger.info("transplantor_3_4 is not ready to move in source tray")
+                time.sleep(2)
+                pass
+        except: 
+            logger.info("Not able to create BufferToTransplantorJob")
+
 
     @staticmethod 
     def create_job(id = -1, src_buffer = None, dst_tranplantor = None ): 
